ae8c83943552e6765ff1e7dfc63c3c49f426a361
Mona Lisa <patrinoua@gmail.com>
2020-12-31 05:27:40 +0200

    absolute imports

--- src/App/MenuAppBar/index.tsx
+++ src/App/MenuAppBar/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { makeStyles } from '@material-ui/core/styles'
-import colors from '../../styleguide/colors'
-import { H5 } from '../../styleguide/typography'
+import colors from 'styleguide/colors'
+import { H5 } from 'styleguide/typography'
+
 import {
   SupportText,
   SupportIcon,
