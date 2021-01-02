1f697d08f15fa45f038bb0d4fd699d14687207b1
Mona Lisa <patrinoua@gmail.com>
2021-01-02 04:23:37 +0200

    colors in typography, add more font-families

--- src/styleguide/typography/index.tsx
+++ src/styleguide/typography/index.tsx
@@ -1,52 +1,49 @@
 import styled from 'styled-components'
+import colors from 'styleguide/colors'
 
 export const H3 = styled.span`
-  //styleName: H3 - Roboto / 24 / Bold / title;
-  font-family: Roboto;
+  font-family: Roboto, Palatino Linotype, sans-serif;
   font-size: 24px;
   font-style: normal;
   font-weight: 700;
   line-height: 32px;
   letter-spacing: 0px;
   text-align: left;
+  color: ${colors.grey500};
 `
 
 export const H4 = styled.span`
-  //styleName: H4 - Roboto / 20 / Bold / title;
-  font-family: Roboto;
+  font-family: Roboto, Palatino Linotype, sans-serif;
   font-size: 20px;
   font-style: normal;
   font-weight: 700;
   line-height: 24px;
   letter-spacing: 0px;
   text-align: left;
+  color: ${colors.grey500};
 `
 export const H5 = styled.span`
-  //styleName: H5 - Roboto / 16 / Bold / title;
-  font-family: Roboto;
+  font-family: Roboto, Palatino Linotype, sans-serif;
   font-size: 1.1em;
   font-style: normal;
   font-weight: 700;
   line-height: 24px;
   letter-spacing: 0px;
   text-align: center;
+  color: ${colors.grey500};
 `
 export const Body1 = styled.div<{ color?: string }>`
-  /* Body 1 - Roboto / 16 / Regular */
-  font-family: Roboto;
+  font-family: Roboto, Palatino Linotype, sans-serif;
   font-style: normal;
   font-weight: normal;
   font-size: 16px;
   line-height: 24px;
-  /* identical to box height, or 150% */
-
   text-align: center;
-
-  color: #4a4a4a;
+  color: ${colors.grey500};
 `
 export const Body2 = styled.div`
   //styleName: Body 2 - Roboto / 14 / Regular;
-  font-family: Roboto;
+  font-family: Roboto, Palatino Linotype, sans-serif;
   font-size: 14px;
   font-style: normal;
   font-weight: 400;
